d986ebee30a347c104cf15977550eff4d5e20828
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-08-21 01:24:53 +0500

    add

--- src/Components/Slideer/Slideer.jsx
+++ src/Components/Slideer/Slideer.jsx
@@ -14,6 +14,16 @@ import card7 from '../../assets/17.jpg';
 import card8 from '../../assets/18.jpg';
 import card9 from '../../assets/19.jpg';
 import card10 from '../../assets/20.jpg';
+import card11 from '../../assets/21.jpg';
+import card12 from '../../assets/22.jpg';
+import card13 from '../../assets/23.jpg';
+import card14 from '../../assets/24.jpg';
+import card15 from '../../assets/25.jpg';
+import card16 from '../../assets/26.jpg';
+import card17 from '../../assets/27.jpg';
+import card18 from '../../assets/28.jpg';
+import card19 from '../../assets/29.jpg';
+import card20 from '../../assets/30.jpg';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
@@ -27,7 +37,17 @@ const data = [
   { id: 7, img: card7, titleKey: "title7" },
   { id: 8, img: card8, titleKey: "title8" },
   { id: 9, img: card9, titleKey: "title9" },
-  { id: 10, img: card10, titleKey: "title10" }
+  { id: 10, img: card10, titleKey: "title10" },
+  { id: 11, img: card11, titleKey: "title11" },
+  { id: 12, img: card12, titleKey: "title12" },
+  { id: 13, img: card13, titleKey: "title13" },
+  { id: 14, img: card14, titleKey: "title14" },
+  { id: 15, img: card15, titleKey: "title15" },
+  { id: 16, img: card16, titleKey: "title16" },
+  { id: 17, img: card17, titleKey: "title17" },
+  { id: 18, img: card18, titleKey: "title18" },
+  { id: 19, img: card19, titleKey: "title19" },
+  { id: 20, img: card20, titleKey: "title20" }
 ];
 
 const Slideer = () => {
