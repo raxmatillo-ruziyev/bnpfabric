53cd1a7fa638df3366827d8a46b0453b245f2bf2
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-08-21 02:07:42 +0500

    add

--- src/Components/Toplam/Toplam.jsx
+++ src/Components/Toplam/Toplam.jsx
@@ -1,10 +1,17 @@
 import { t } from 'i18next'
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Toplam.scss'
 import { Link, Outlet } from 'react-router-dom'
 import Service2 from '../Service/Service2'
+import { useTranslation } from 'react-i18next'
 
 const Toplam = () => {
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const savedLanguage = localStorage.getItem('i18nextLng') || 'uz';
+        i18n.changeLanguage(savedLanguage);
+    }, []);
   return (
     <>
     <div className="container">
